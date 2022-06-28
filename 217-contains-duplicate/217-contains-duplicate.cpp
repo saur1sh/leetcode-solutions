7ce225b3d5c037df55b6aaef5f9e8487fb4f2319
Saurish <71913696+Saur1sh@users.noreply.github.com>
2022-06-28 14:42:37 +0530

    Time: 148 ms (42.04%), Space: 51.5 MB (51.00%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+#include<unordered_set>
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for(int i=0; i<nums.size(); i++){
+            if(st.find(nums[i])==st.end())
+                st.insert(nums[i]);
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
