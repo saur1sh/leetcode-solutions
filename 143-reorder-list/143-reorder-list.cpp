6c0385e58d0d5d638537e60b2b739d7abb71609f
Saurish <71913696+Saur1sh@users.noreply.github.com>
2022-07-06 04:12:15 +0530

    Time: 54 ms (65.16%), Space: 17.7 MB (95.63%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -11,40 +11,40 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        ListNode* front=head;
-        ListNode* back=head;
-        int n=1;
-        while(back->next!=NULL){
-        	back = back->next;
-        	n++;
-        }
-        if(n%2==0)
-        	n=n/2;
-        else
-        	n = (n+1)/2;
-        back = head;
-        int i=1;
-        while(i!=n){
-        	back = back->next;
-        	i++;
-        }
-        ListNode* curr = back;
-        ListNode* prev = NULL;
-        ListNode* next = NULL;
-        while(curr!=NULL){
-        	next = curr->next;
-        	curr->next = prev;
-        	prev = curr;
-        	curr = next;
-        }
+		ListNode* front = head;
+		ListNode* back = head;
+		int n = 1;
+		while (back->next != NULL) {
+			back = back->next;
+			n++;
+		}
+		if (n % 2 == 0)
+			n = n / 2;
+		else
+			n = (n + 1) / 2;
+		back = head;
+		int i = 1;
+		while (i != n) {
+			back = back->next;
+			i++;
+		}
+		ListNode* curr = back;
+		ListNode* prev = NULL;
+		ListNode* next = NULL;
+		while (curr != NULL) {
+			next = curr->next;
+			curr->next = prev;
+			prev = curr;
+			curr = next;
+		}
 
-        for(i=0; i<n; i++){
-        	curr = front->next;
-        	front->next = prev;
-        	front = curr;
-        	next = prev->next;
-        	prev->next = curr;
-        	prev = next;
-        }
-    }
+		for (i = 0; i < n; i++) {
+			curr = front->next;
+			front->next = prev;
+			front = curr;
+			next = prev->next;
+			prev->next = curr;
+			prev = next;
+		}
+	}
 };
