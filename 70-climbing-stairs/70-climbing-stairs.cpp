462424da043a9c824e4da2a7d1df611f0f4ac75f
Saurish <71913696+Saur1sh@users.noreply.github.com>
2022-07-09 05:47:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (56.44%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int climbStairs(int n){
-        int temp, one=1, two=1;
-        for(int i=0; i<n-1; i++){
+    int climbStairs(int n) {
+        if(n==1)
+            return 1;
+        int temp, one=1, two=1, ans;
+        for(int i=n-2; i>=0; i--){
+            ans = one + two;
             temp = one;
-            one = one + two;
+            one = ans;
             two = temp;
         }
-        
-        return one;
+        return ans;
     }
 };
