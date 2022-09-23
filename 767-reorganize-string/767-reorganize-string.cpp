8273200743e22086e01335d34cf38888654b61a5
Saurish <71913696+Saur1sh@users.noreply.github.com>
2022-09-23 18:49:53 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (35.28%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int n=s.length();
+        unordered_map <char, int> mp;
+        for(auto &c: s){
+            ++mp[c];
+            if(mp[c]>(n+1)/2) return "";
+        }
+        
+        priority_queue<pair<int, char>> pq;
+        for(auto c: mp){
+            pq.push({c.second, c.first});
+        }
+        
+        pair<int, char> prevMax = {0, '#'};
+        string ans = "";
+        while(pq.size()){
+            pair<int, char> temp = pq.top();
+            pq.pop();
+            ans+=temp.second;
+            temp.first--;
+            if(prevMax.first>0){
+                pq.push(prevMax);
+            }
+            prevMax = temp;
+        }
+        if(ans.length()==n) return ans;
+        return "";
+    }
+};
