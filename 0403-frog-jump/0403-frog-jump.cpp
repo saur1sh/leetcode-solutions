7f316c9d1453392387dbdbb0035993b4ac09e42f
Saurish <isaurishsharma@gmail.com>
2023-08-27 20:37:45 +0530

    Time: 60 ms (89.71%), Space: 27.1 MB (80.91%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,16 +1,46 @@
 class Solution {
 public:
 
-    bool canCross(vector<int>& stones) {
-        unordered_map<int, set<int>> mp;
-        mp[1] = {1};
-        for(int i=1; i<stones.size(); i++) {
-            for(auto jumpSize: mp[stones[i]]) {
-                mp[stones[i] + jumpSize].insert(jumpSize);
-                mp[stones[i] + jumpSize+1].insert(jumpSize+1);
-                mp[stones[i] + jumpSize-1].insert(jumpSize-1);
+    map<pair<int, int>, bool> mp;
+
+    bool solve(int ind, int k, vector<int> &st) {
+        if(ind==st.size()-1) {
+            return true;
+        }
+        if(ind>=st.size())
+            return false;
+        if(mp.find({ind, k})!=mp.end())
+            return mp[{ind, k}];
+        int canJump[3];
+        memset(canJump, -1, sizeof(canJump));
+        int sizes[3] = {k-1, k, k+1};
+        for(int i=ind+1; i<st.size(); i++) {
+            int diff = st[i]-st[ind];
+            if(diff>k+1)
+                break;
+            if(diff==k-1){
+                canJump[0] = i;
+            }
+            else if(diff==k){
+                canJump[1] = i;
+            }
+            else if(diff==k+1){
+                canJump[2] = i;
             }
         }
-        return mp[stones.back()].size()!=0;
+        for(int i=0; i<3; i++) {
+            if(canJump[i]!=-1) {
+                if(solve(canJump[i], sizes[i], st)){
+                    return mp[{ind, k}] = true;
+                }
+            }
+        }
+        return mp[{ind, k}] = false;
+    }
+
+    bool canCross(vector<int>& stones) {
+        if(stones[1]>1)
+            return false;
+        return solve(1, 1, stones);
     }
 };
