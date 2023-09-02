fc6682a7fbba366498c61d56e77b72e220f1b545
Saurish <isaurishsharma@gmail.com>
2023-09-02 15:15:34 +0530

    Time: 2073 ms (37.28%), Space: 0B (100%) - LeetHub

--- 1541-top-travellers/1541-top-travellers.sql
+++ 1541-top-travellers/1541-top-travellers.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select u.name, if(isnull(sum(r.distance)), 0, sum(r.distance)) as travelled_distance
+select u.name, ifnull(sum(r.distance), 0) as travelled_distance
 from users u
 left join rides r
 on r.user_id=u.id
