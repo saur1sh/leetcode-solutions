92ec878c99a8e5d12a66a45bc8487ccee42e488b
Saurish <isaurishsharma@gmail.com>
2023-09-03 18:22:37 +0530

    Time: 219 ms (41.71%), Space: 80.5 MB (58.61%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int minExtraChar(string s, vector<string>& dictionary) {
-        int maxi = s.length() + 1;
-        int dp[55];
+        int n=s.length();
+        int maxi = n+1;
+        int dp[52];
         memset(dp, maxi, sizeof(dp));
         dp[0] = 0;
-
-        unordered_set<string> dictionary_set(dictionary.begin(), dictionary.end());
-
-        for (int i = 1; i <= s.length(); ++i) {
-            dp[i] = dp[i - 1] + 1;
-            for (int l = 1; l <= i; l++) {
-                if (dictionary_set.find(s.substr(i - l, l)) != dictionary_set.end()) {
-                    dp[i] = min(dp[i], dp[i - l]);
+        set<string> st (dictionary.begin(), dictionary.end());
+        for(int i=1; i<=s.length(); i++) {
+            dp[i] = dp[i-1] +1;
+            for(int l=1; l<=i; l++) {
+                if(st.find(s.substr(i-l, l))!=st.end()) {
+                    dp[i] = min(dp[i], dp[i-l]);
                 }
             }
         }
-        return dp[s.length()];
+        return dp[n];
     }
 };
