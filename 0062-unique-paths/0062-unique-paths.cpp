e7517c366bd4cfcb2980ab857f0ad9d184163def
Saurish <isaurishsharma@gmail.com>
2023-09-03 08:17:36 +0530

    Time: 0 ms (100%), Space: 6.2 MB (71.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
 
-    int solve(int i, int j, int m, int n, int dp[101][101]) {
-        if(i>=m || j>=n)
-            return 0;
-        if(i==m-1 && j==n-1)
-            return 1;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int right = solve(i, j+1, m, n, dp);
-        int down = solve(i+1, j, m, n, dp);
-        return dp[i][j] = right+down;
+    int nCr(int n, int r) {
+        if (r > n) return 0;
+        if (r == 0 || n == r) return 1;
+        double res = 0;
+        for (int i = 0; i < r; i++) {
+            res += log(n-i) - log(i+1);
+        }
+        return (int)round(exp(res));
     }
 
     int uniquePaths(int m, int n) {
-        int dp[101][101];
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, m, n, dp);
+        return nCr(n+m-2, m-1);
     }
 };
