6a2d7378ba20397ebaf62004b29f1667c8daf5ff
Saurish <isaurishsharma@gmail.com>
2023-09-03 08:20:27 +0530

    Time: 2 ms (44.36%), Space: 6.2 MB (67.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
 
-    int nCr(int n, int r) {
-        if (r > n) return 0;
-        if (r == 0 || n == r) return 1;
+    int uniquePaths(int m, int n) {
+        int N = m+n-2;
+        int r = m-1;
+        if(r>N) return 0;
+        if(r==0 || N==r) return 1;
         double res = 0;
         for (int i = 0; i < r; i++) {
-            res += log(n-i) - log(i+1);
+            res += log(N-i) - log(i+1);
         }
         return (int)round(exp(res));
-    }
-
-    int uniquePaths(int m, int n) {
-        return nCr(n+m-2, m-1);
     }
 };
