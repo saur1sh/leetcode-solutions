6c4bd4ca0492c0e9aecdb20b1231b2cba0f43878
Saurish <isaurishsharma@gmail.com>
2023-09-09 08:54:40 +0530

    Time: 3 ms (50.37%), Space: 6.5 MB (85.45%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
 
-    int solve(int tar, vector<int> &nums, int dp[1001]) {
+    int dp[1001];
+
+    int solve(int tar, vector<int> &nums) {
         if(tar==0) {
             return 1;
         }
@@ -12,7 +14,7 @@ public:
             return dp[tar];
         int combs = 0;
         for(int i=0; i<nums.size(); i++) {
-            int res = solve(tar-nums[i], nums, dp);
+            int res = solve(tar-nums[i], nums);
             if(res>0) {
                 combs+=res;
             }
@@ -21,8 +23,7 @@ public:
     }
 
     int combinationSum4(vector<int>& nums, int target) {
-        int dp[1001];
         memset(dp, -1, sizeof(dp));
-        return solve(target, nums, dp);
+        return solve(target, nums);
     }
 };
