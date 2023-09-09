df714b7777c165fe7b0021fd742ccaa4dc58e2d3
Saurish <isaurishsharma@gmail.com>
2023-09-09 08:53:27 +0530

    Time: 3 ms (50.37%), Space: 6.7 MB (46.24%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
 
-    int combinationSum4(vector<int>& nums, int target) {
-        unsigned int dp[1001];
-        memset(dp, 0, sizeof(dp));
-        dp[0] = 1;
-        for(int i=1; i<=target; i++) {
-            for(const auto &e: nums) {
-                if(i-e>=0) {
-                    dp[i] += dp[i-e];
-                }
+    int solve(int tar, vector<int> &nums, int dp[1001]) {
+        if(tar==0) {
+            return 1;
+        }
+        else if(tar<0) {
+            return 0;
+        }
+        if(dp[tar]!=-1)
+            return dp[tar];
+        int combs = 0;
+        for(int i=0; i<nums.size(); i++) {
+            int res = solve(tar-nums[i], nums, dp);
+            if(res>0) {
+                combs+=res;
             }
         }
-        return dp[target];
+        return dp[tar] = combs;
+    }
+
+    int combinationSum4(vector<int>& nums, int target) {
+        int dp[1001];
+        memset(dp, -1, sizeof(dp));
+        return solve(target, nums, dp);
     }
 };
