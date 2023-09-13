fcbeff8ac384245d96f8d54abe7455f430af0f43
Saurish <isaurishsharma@gmail.com>
2023-09-13 20:51:21 +0530

    Time: 54 ms (91.53%), Space: 14.7 MB (81.41%) - LeetHub

--- 2044-number-of-wonderful-substrings/2044-number-of-wonderful-substrings.cpp
+++ 2044-number-of-wonderful-substrings/2044-number-of-wonderful-substrings.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     long long wonderfulSubstrings(string word) {
-        int count[1024];
-        memset(count, 0, sizeof(count));
+        int count[1024] = {};
         count[0] = 1;
         int mask=0;
         long long res = 0;
-        for(const auto &e: word) {
+        for(const char &e: word) {
             mask ^= 1<<(e-'a');
             res += count[mask];
             for(int i=0; i<10; i++) {
