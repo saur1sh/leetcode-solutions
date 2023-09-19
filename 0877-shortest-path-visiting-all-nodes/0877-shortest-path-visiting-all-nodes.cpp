92fba6acd35c4a183c63257f4460f43032181852
Saurish <isaurishsharma@gmail.com>
2023-09-19 15:20:17 +0530

    Time: 48 ms (52.03%), Space: 18.1 MB (48.82%) - LeetHub

--- 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.cpp
+++ 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.cpp
@@ -5,13 +5,13 @@ class Solution {
 
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
-        int n=graph.size();
+        int n = graph.size();
         int fina = (1<<n)-1;
         queue<states> q;
         unordered_set<int> vis;
         for(int i=0; i<n; i++) {
             q.push({1<<i, i, 0});
-            vis.insert((1<<i)*16 + i);
+            vis.insert((1<<i)*16+i);
         }
         while(!q.empty()) {
             states curr = q.front();
@@ -19,12 +19,12 @@ public:
             if(curr.mask==fina) {
                 return curr.dist;
             }
-            for(const int &e: graph[curr.node]) {
+            for(const int &e: graph[curr.node]){
                 int newMask = curr.mask | (1<<e);
-                int hashValue = newMask*16+e;
-                if(vis.find(hashValue)==vis.end()) {
-                    vis.insert(hashValue);
+                int hashVal = newMask*16+e;
+                if(vis.find(hashVal)==vis.end()) {
                     q.push({newMask, e, curr.dist+1});
+                    vis.insert(hashVal);
                 }
             }
         }
