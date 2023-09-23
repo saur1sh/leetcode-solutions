8b08d0c5556e1c58ee376e81e94dced55a96c5e6
Saurish <isaurishsharma@gmail.com>
2023-09-23 16:17:54 +0530

    Time: 159 ms (74.99%), Space: 10.7 MB (46.75%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -17,9 +17,6 @@ public:
                 dp[i] = 1 + maxi;
             ans = max(dp[i], ans);
         }
-        for(int i=0; i<nums.size(); i++) {
-          cout<<dp[i]<<" ";
-        }
         return ans;
     }
 };
