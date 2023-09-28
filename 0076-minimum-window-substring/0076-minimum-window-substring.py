0caeda42a28de52d7882ccc74fe311c65aa8301a
Saurish <isaurishsharma@gmail.com>
2023-09-28 20:54:47 +0530

    Time: 107 ms (70.24%), Space: 17 MB (85.87%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,30 +2,30 @@ from collections import defaultdict
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        mp = defaultdict(int)
-        mp2 = defaultdict(int)
-        
+        mp = [0] * 128
+        l = r = 0
+        matches = 0
+        minLen = float('inf')
+        minStart = 0
+
         for c in t:
-            mp[c] += 1
-        l = 0
-        r = 0
-        matches = len(mp)
-        
-        ans = s + "1"
-        counter = 0
-        
+            mp[ord(c)] += 1
+
         while r < len(s):
-            mp2[s[r]] += 1
-            if mp2[s[r]] == mp[s[r]]:
-                counter += 1
-            if counter == matches:
-                while counter == matches:
-                    if r - l + 1 < len(ans):
-                        ans = s[l:r + 1]
-                    if mp[s[l]] == mp2[s[l]]:
-                        counter -= 1
-                    mp2[s[l]] -= 1
-                    l += 1
+            if mp[ord(s[r])] > 0:
+                matches += 1
+            mp[ord(s[r])] -= 1
+
+            while matches == len(t):
+                if r - l + 1 < minLen:
+                    minLen = r - l + 1
+                    minStart = l
+
+                mp[ord(s[l])] += 1
+                if mp[ord(s[l])] > 0:
+                    matches -= 1
+                l += 1
+
             r += 1
-        
-        return "" if ans[-1] == '1' else ans
+
+        return "" if minLen == float('inf') else s[minStart:minStart + minLen]
