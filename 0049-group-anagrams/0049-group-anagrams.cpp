de8796baaac64c6452343b6f67a6dfae67d5286b
Saurish <isaurishsharma@gmail.com>
2023-09-28 09:41:35 +0530

    Time: 32 ms (51.44%), Space: 19.8 MB (78.83%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,27 @@
+#include <functional>
+class Solution {
+public:
+
+    size_t hashAnagram(const string& str) {
+        string sortedStr = str;
+        sort(sortedStr.begin(), sortedStr.end());
+        return hash<string>{}(sortedStr);
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<size_t, vector<int>> mp;
+        for(int i=0; i<strs.size(); i++) {
+            unsigned long long x = hashAnagram(strs[i]);
+            mp[x].push_back(i);
+        }
+        vector<vector<string>> ans;
+        for(auto &it: mp) {
+            vector<string> temp;
+            for(const auto &it2: it.second) {
+                temp.push_back(strs[it2]);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
