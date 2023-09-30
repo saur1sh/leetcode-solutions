cb94cd3537232bd7da384ab8e56b06e6eb61de9f
Saurish <isaurishsharma@gmail.com>
2023-09-30 11:11:31 +0530

    Time: 449 ms (75.42%), Space: 116 MB (64.05%) - LeetHub

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/1557-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/1557-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -11,7 +11,7 @@ public:
     bool hasAllCodes(string s, int k) {
         if(s.size()<=k)
             return false;
-        set<string> st;
+        unordered_set<string> st;
         for(int i=0; i<s.length()-k+1; i++) {
             st.insert(s.substr(i, k));
         }
