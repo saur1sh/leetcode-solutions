3b536ccebcdaa1e86a8c7ef25236b3ddca2d4bf8
Saurish <isaurishsharma@gmail.com>
2023-10-10 20:27:07 +0530

    Time: 199 ms (55.02%), Space: 64.5 MB (71.55%) - LeetHub

--- 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -6,8 +6,7 @@ public:
         nums.erase(unique(nums.begin(), nums.end()), nums.end());
         int r = 0, n = nums.size();
         for(int l=0; l<n; l++) {
-            while(r<n && nums[r]<nums[l]+m)
-                r++;
+            r = upper_bound(nums.begin(), nums.end(), nums[l]+m-1)-nums.begin();
             res = min(res, m-(r-l));
         }
         return res;
