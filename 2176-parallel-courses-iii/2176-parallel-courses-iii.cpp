4db0791c78da40acae58124fe1ffe7b2fd4c46f8
Saurish <isaurishsharma@gmail.com>
2023-10-18 08:29:49 +0530

    Time: 311 ms (77.7%), Space: 129.8 MB (92.09%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
@@ -8,22 +8,20 @@ public:
             ind[e[1]]++;
         }
         int maxi = 0;
-        vector<pair<int, int>> vis;
+        vector<int> vis(n+1);
         queue<pair<int, int>> q;
         for(int i=0; i<n+1; i++) {
             if(i!=0 && ind[i]==0)
                 q.push({i, time[i-1]});
-            vis.push_back({0, 0});
         }
         while(!q.empty()) {
             int u = q.front().first;
             int cst = q.front().second;
             q.pop();
             for(const auto &v: adj[u]) {
-                vis[v].first++;
-                vis[v].second = max(vis[v].second, cst);
-                if(ind[v]==vis[v].first)
-                    q.push({v, vis[v].second+time[v-1]});
+                vis[v] = max(vis[v], cst);
+                if(--ind[v]==0)
+                    q.push({v, vis[v]+time[v-1]});
             }
             maxi = (cst>maxi) ? cst : maxi ;
         }
