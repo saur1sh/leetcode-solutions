426667077dc280bb973910a2b03a9fecaf1c9671
Saurish <isaurishsharma@gmail.com>
2023-10-18 08:15:37 +0530

    Time: 315 ms (72.3%), Space: 132.8 MB (64.93%) - LeetHub

---
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimumTime(int n, vector<vector<int>>& rel, vector<int>& time) {
+        vector<int> adj[n+1];
+        vector<int> ind(n+1, 0);
+        for(const auto &e: rel) {
+            adj[e[0]].push_back(e[1]);
+            ind[e[1]]++;
+        }
+        int maxi = 0;
+        vector<pair<int, int>> vis;
+        queue<pair<int, int>> q;
+        for(int i=0; i<ind.size(); i++) {
+            if(i!=0 && ind[i]==0)
+                q.push({i, time[i-1]});
+            vis.push_back({0, 0});
+        }
+        while(!q.empty()) {
+            int u = q.front().first;
+            int cst = q.front().second;
+            q.pop();
+            for(const auto &v: adj[u]) {
+                vis[v].first++;
+                vis[v].second = max(vis[v].second, cst);
+                if(ind[v]==vis[v].first)
+                    q.push({v, vis[v].second+time[v-1]});
+            }
+            maxi = max(maxi, cst);
+        }
+        return maxi;
+    }
+};
