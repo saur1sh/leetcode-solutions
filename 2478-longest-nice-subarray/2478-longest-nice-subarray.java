b3b5641959a323f72fbd8b9f92c03caf74c23547
Saurish <isaurishsharma@gmail.com>
2025-03-19 11:42:34 +0530

    Time: 4 ms (28.65%), Space: 55.6 MB (77.45%) - LeetHub

---
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int curr = 0;
+        int maxi = 0;
+        int l = 0, r = 0;
+        while (r<nums.length) {
+            int check = curr&nums[r];
+            if (check==0) {
+                curr ^= nums[r];
+                maxi = Math.max(maxi, r-l+1);
+                r++;
+            }
+            else {
+                curr ^= nums[l];
+                l++;
+            }
+        }
+        return maxi;
+    }
+}
