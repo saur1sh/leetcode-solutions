38a633bd68f2902aa06a90d58bc450c6bf8732e2
Saurish <isaurishsharma@gmail.com>
2025-09-10 10:22:51 +0530

    Time: 0 ms (100%), Space: 71.6 MB (97.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,12 @@
-int speedUp = []
-{std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }();
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int maxi = nums[0];
         int curr = 0;
         for(const int &e: nums) {
-          if(curr<0) curr = e;
-          else curr += e;
-          maxi = max(curr, maxi);
+            if(curr<0) curr = e;
+            else curr +=e;
+            maxi = max(curr, maxi);
         }
         return maxi;
     }
