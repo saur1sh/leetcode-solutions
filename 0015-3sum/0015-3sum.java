23cfda6823e7fe419fc339724772e03816bcb05e
Saurish <isaurishsharma@gmail.com>
2025-09-21 18:33:46 +0530

    Time: 31 ms (56.6%), Space: 51.6 MB (46.77%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,39 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        int tar = 0;
         List<List<Integer>> ans = new ArrayList<>();
-        HashSet<Long> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            int ntar = tar - nums[i];
-            int li = i + 1;
-            int ri = nums.length - 1;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int li = i + 1, ri = nums.length - 1;
             while (li < ri) {
-                int sum = nums[li] + nums[ri];
-                if (sum > ntar) {
+                int sum = nums[i] + nums[li] + nums[ri];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[li], nums[ri]));
+                    li++;
                     ri--;
-                } else if (sum < ntar) {
+                    while (li < ri && nums[li] == nums[li - 1]) li++;
+                    while (li < ri && nums[ri] == nums[ri + 1]) ri--;
+                } else if (sum < 0) {
                     li++;
                 } else {
-                    long hash = getHash(nums[i], nums[li], nums[ri]);
-                    if (!set.contains(hash)) {
-                        ans.add(List.of(nums[i], nums[li], nums[ri]));
-                        set.add(hash);
-                    }
-                    li++;
                     ri--;
                 }
             }
         }
         return ans;
-    }
-
-    private long getHash(int x, int y, int z) {
-        long hash = x;
-        hash *= 100000;
-        hash += y;
-        hash *= 100000;
-        hash += z;
-        return hash;
     }
 }
