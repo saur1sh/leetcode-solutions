a32cc0c923518cbb38bf8d91481002d23aea213b
Saurish <isaurishsharma@gmail.com>
2025-09-21 18:36:42 +0530

    Time: 25 ms (96.44%), Space: 51.5 MB (70%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,23 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
+        int tar = 0;
         List<List<Integer>> ans = new ArrayList<>();
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int li = i + 1, ri = nums.length - 1;
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int li = i + 1;
+            int ri = nums.length - 1;
+            int ntar = tar - nums[i];
             while (li < ri) {
-                int sum = nums[i] + nums[li] + nums[ri];
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[li], nums[ri]));
-                    li++;
+                int sum = nums[li] + nums[ri];
+                if (sum > ntar) {
                     ri--;
-                    while (li < ri && nums[li] == nums[li - 1]) li++;
-                    while (li < ri && nums[ri] == nums[ri + 1]) ri--;
-                } else if (sum < 0) {
+                } else if (sum < ntar) {
                     li++;
                 } else {
+                    ans.add(List.of(nums[i], nums[li], nums[ri]));
+                    li++;
                     ri--;
+                    while (li < ri && nums[li] == nums[li - 1])
+                        li++;
+                    while (li < ri && nums[ri] == nums[ri + 1])
+                        ri--;
                 }
             }
         }
