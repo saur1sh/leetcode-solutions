fdebe55002e8d6fc4f0cd04a5743e3f40a746224
Saurish <isaurishsharma@gmail.com>
2025-12-10 09:59:32 +0530

    Time: 2 ms (74.67%), Space: 92.1 MB (13.33%) - LeetHub

---
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.java
@@ -0,0 +1,15 @@
+class Solution {
+    private final int MOD = 1000000007;
+
+    public int countPermutations(int[] comp) {
+        long ans = 1;
+        int n = comp.length;
+        for(int i=1; i<n; i++) {
+            if(comp[i]<=comp[0]) {
+                return 0;
+            }
+            ans = (ans * i) % MOD;
+        }
+        return (int) ans;
+    }
+}
