89323edd78851042ea4152db5821205d07225c0f
Saurish <isaurishsharma@gmail.com>
2025-12-11 11:03:25 +0530

    Time: 134 ms (63.16%), Space: 278.6 MB (17.9%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.java
+++ 3531-count-covered-buildings/3531-count-covered-buildings.java
@@ -3,12 +3,20 @@ class Solution {
         if (n < 3) {
             return 0;
         }
-        HashMap<Integer, TreeSet<Integer>> ym = new HashMap<>();
-        HashMap<Integer, TreeSet<Integer>> xm = new HashMap<>();
+        HashMap<Integer, int[]> ym = new HashMap<>();
+        HashMap<Integer, int[]> xm = new HashMap<>();
 
         for (int i = 0; i < build.length; i++) {
-            xm.computeIfAbsent(build[i][0], h -> new TreeSet()).add(build[i][1]);
-            ym.computeIfAbsent(build[i][1], h -> new TreeSet()).add(build[i][0]);
+            int x = build[i][0];
+            int y = build[i][1];
+            int[] aX = xm.computeIfAbsent(x, h -> new int[] { Integer.MAX_VALUE, Integer.MIN_VALUE });
+            int[] aY = ym.computeIfAbsent(y, h -> new int[] { Integer.MAX_VALUE, Integer.MIN_VALUE });
+
+            aX[0] = Math.min(aX[0], y);
+            aX[1] = Math.max(aX[1], y);
+
+            aY[0] = Math.min(aY[0], x);
+            aY[1] = Math.max(aY[1], x);
         }
 
         int ans = 0;
@@ -28,8 +36,8 @@ class Solution {
         return ans;
     }
 
-    public static boolean isMiddle(TreeSet<Integer> set, int value) {
-        if (value > set.first() && value < set.last()) {
+    public static boolean isMiddle(int[] arr, int value) {
+        if (value > arr[0] && value < arr[1]) {
             return true;
         }
         return false;
