062547afd88e29e92f54dae60708245861fc740c
Saurish <isaurishsharma@gmail.com>
2026-08-20 10:32:50 +0530

    Time: 1 ms (98.08%), Space: 46.7 MB (66.35%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -1,26 +1,22 @@
 class Solution {
     public int[] resultArray(int[] nums) {
-        ArrayList<Integer> arr1 = new ArrayList<>();
-        ArrayList<Integer> arr2 = new ArrayList<>();
-        arr1.add(nums[0]);
-        arr2.add(nums[1]);
-        for(int i=2; i<nums.length; i++) {
-            if(arr1.get(arr1.size()-1) > arr2.get(arr2.size()-1)) {
-                arr1.add(nums[i]);
-            }
-            else {
-                arr2.add(nums[i]);
-            }
-        }
-        int id = 0;
-        for(int i=0; i<nums.length; i++) {
-            if(i<=arr1.size()-1) {
-                nums[i] = arr1.get(i);
-            }
-            else {
-                nums[i] = arr2.get(id++);
+        int n = nums.length;
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+        int idx1 = 0;
+        int idx2 = 0;
+        arr1[idx1++] = nums[0];
+        arr2[idx2++] = nums[1];
+        for (int i = 2; i < nums.length; i++) {
+            if (arr1[idx1 - 1] > arr2[idx2 - 1]) {
+                arr1[idx1++] = nums[i];
+            } else {
+                arr2[idx2++] = nums[i];
             }
         }
+        System.arraycopy(arr1, 0, nums, 0, idx1);
+        System.arraycopy(arr2, 0, nums, idx1, idx2);
+
         return nums;
     }
 }
