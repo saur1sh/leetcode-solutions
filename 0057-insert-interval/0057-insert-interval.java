967ab2d41c678fc18f1ba992afbd75a44cbaf6ea
Saurish <isaurishsharma@gmail.com>
2026-08-23 15:45:17 +0530

    Time: 1 ms (98.15%), Space: 47.3 MB (19.97%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,25 +1,26 @@
 class Solution {
     public int[][] insert(int[][] arr, int[] newInterval) {
-        int[][] ans = new int[arr.length + 1][2];
+        int[][] ans = new int[arr.length + 1][]; 
         int id = 0;
         int i = 0;
+        
         while (i < arr.length && arr[i][1] < newInterval[0]) {
-            ans[id][0] = arr[i][0];
-            ans[id++][1] = arr[i][1];
-            i++;
+            ans[id++] = arr[i++];
         }
+        
         while (i < arr.length && arr[i][0] <= newInterval[1]) {
             newInterval[0] = Math.min(arr[i][0], newInterval[0]);
             newInterval[1] = Math.max(arr[i][1], newInterval[1]);
             i++;
         }
-        ans[id][0] = newInterval[0];
-        ans[id++][1] = newInterval[1];
-        while (i < arr.length) {
-            ans[id][0] = arr[i][0];
-            ans[id++][1] = arr[i][1];
-            i++;
+        ans[id++] = newInterval;
+        
+        if (i < arr.length) {
+            int remaining = arr.length - i;
+            System.arraycopy(arr, i, ans, id, remaining);
+            id += remaining;
         }
+        
         return Arrays.copyOf(ans, id);
     }
 }
