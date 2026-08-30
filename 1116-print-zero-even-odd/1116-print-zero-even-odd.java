35d18fe8a8dc04142cc8a3187126d61b9deea819
Saurish <isaurishsharma@gmail.com>
2026-08-30 11:03:12 +0530

    Time: 6 ms (21.77%), Space: 43.1 MB (99.61%) - LeetHub

--- 1116-print-zero-even-odd/1116-print-zero-even-odd.java
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.java
@@ -1,40 +1,42 @@
 class ZeroEvenOdd {
     private int n;
-    private Semaphore zero, odd, even;
+    private int state = 0;
 
     public ZeroEvenOdd(int n) {
         this.n = n;
-        this.zero = new Semaphore(1);
-        this.odd = new Semaphore(0);
-        this.even = new Semaphore(0);
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
-    public void zero(IntConsumer printNumber) throws InterruptedException {
+    public synchronized void zero(IntConsumer printNumber) throws InterruptedException {
         for (int i = 1; i <= n; i++) {
-            zero.acquire();
-            printNumber.accept(0);
-            if (i % 2 == 0) {
-                even.release();
-            } else {
-                odd.release();
+            while (state != 0) {
+                wait();
             }
+            printNumber.accept(0);
+            state = (i % 2 == 0) ? 2 : 1;
+            notifyAll();
         }
     }
 
-    public void even(IntConsumer printNumber) throws InterruptedException {
+    public synchronized void even(IntConsumer printNumber) throws InterruptedException {
         for (int i = 2; i <= n; i += 2) {
-            even.acquire();
+            while (state != 2) {
+                wait();
+            }
             printNumber.accept(i);
-            zero.release();
+            state = 0;
+            notifyAll();
         }
     }
 
-    public void odd(IntConsumer printNumber) throws InterruptedException {
+    public synchronized void odd(IntConsumer printNumber) throws InterruptedException {
         for (int i = 1; i <= n; i += 2) {
-            odd.acquire();
+            while (state != 1) {
+                wait();
+            }
             printNumber.accept(i);
-            zero.release();
+            state = 0;
+            notifyAll();
         }
     }
 }
