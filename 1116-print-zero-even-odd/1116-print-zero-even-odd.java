771ed41bb43a66b79cfb70d908089bc9a66938dc
Saurish <isaurishsharma@gmail.com>
2026-08-30 11:04:11 +0530

    Time: 5 ms (95.15%), Space: 43.8 MB (85.84%) - LeetHub

--- 1116-print-zero-even-odd/1116-print-zero-even-odd.java
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.java
@@ -1,42 +1,39 @@
 class ZeroEvenOdd {
     private int n;
-    private int state = 0;
+    private volatile int state = 0;
 
     public ZeroEvenOdd(int n) {
         this.n = n;
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
-    public synchronized void zero(IntConsumer printNumber) throws InterruptedException {
+    public void zero(IntConsumer printNumber) throws InterruptedException {
         for (int i = 1; i <= n; i++) {
             while (state != 0) {
-                wait();
+                Thread.onSpinWait();
             }
             printNumber.accept(0);
             state = (i % 2 == 0) ? 2 : 1;
-            notifyAll();
         }
     }
 
-    public synchronized void even(IntConsumer printNumber) throws InterruptedException {
+    public void even(IntConsumer printNumber) throws InterruptedException {
         for (int i = 2; i <= n; i += 2) {
             while (state != 2) {
-                wait();
+                Thread.onSpinWait();
             }
             printNumber.accept(i);
             state = 0;
-            notifyAll();
         }
     }
 
-    public synchronized void odd(IntConsumer printNumber) throws InterruptedException {
+    public void odd(IntConsumer printNumber) throws InterruptedException {
         for (int i = 1; i <= n; i += 2) {
             while (state != 1) {
-                wait();
+                Thread.onSpinWait();
             }
             printNumber.accept(i);
             state = 0;
-            notifyAll();
         }
     }
 }
