fe656102122347bdf78aaff82ae56dbfde0523e2
Saurish <isaurishsharma@gmail.com>
2026-09-20 16:40:06 +0530

    Time: 6 ms (94.7%), Space: 46.6 MB (11.09%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.java
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.java
@@ -1,41 +1,24 @@
 class Solution {
-
-    public static long hashString(String str) {
-        long hash = 0;
-        long p = 31;
-        long mod = (long) (1e9 + 9);
-        long p_pow = 1;
-
-        for (int i = 0; i < str.length(); i++) {
-            long charValue = str.charAt(i) - 'a' + 1;
-            hash = (hash + charValue * p_pow) % mod;
-            p_pow = (p_pow * p) % mod;
-        }
-
-        return hash;
-    }
-
     public int countWords(String[] words1, String[] words2) {
-        HashMap<Long, Integer> mp = new HashMap<>();
-        Set<Long> uniqueIndex = new HashSet<>();
-        for (int i = 0; i < words1.length; i++) {
-            long hash = hashString(words1[i]);
-            mp.put(hash, mp.getOrDefault(hash, 0) + 1);
+        HashMap<String, Integer> mp1 = new HashMap<>();
+        HashMap<String, Integer> mp2 = new HashMap<>();
+
+        for (String word : words1) {
+            mp1.put(word, mp1.getOrDefault(word, 0) + 1);
         }
-        for (int i = 0; i < words2.length; i++) {
-            long hash = hashString(words2[i]);
-            int occur = mp.getOrDefault(hash, 0);
-            if (occur == 0) {
-                continue;
-            }
-            if (occur == 1) {
-                uniqueIndex.add(hash);
-            }
-            if (occur > 1 && uniqueIndex.contains(hash)) {
-                uniqueIndex.remove(hash);
-            }
-            mp.put(hash, occur + 1);
+
+        for (String word : words2) {
+            mp2.put(word, mp2.getOrDefault(word, 0) + 1);
         }
-        return uniqueIndex.size();
+
+        int count = 0;
+
+        for (String word : words1) {
+            if (mp1.get(word) == 1 && mp2.getOrDefault(word, 0) == 1) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
