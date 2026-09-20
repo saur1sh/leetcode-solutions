de35510fac03cdac68e3064f0af1457f5711429e
Saurish <isaurishsharma@gmail.com>
2026-09-20 16:43:39 +0530

    Time: 5 ms (99.51%), Space: 46.2 MB (63.95%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.java
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.java
@@ -1,24 +1,21 @@
 class Solution {
     public int countWords(String[] words1, String[] words2) {
-        HashMap<String, Integer> mp1 = new HashMap<>();
-        HashMap<String, Integer> mp2 = new HashMap<>();
-
+        HashMap<String, Integer> mp = new HashMap<>();
         for (String word : words1) {
-            mp1.put(word, mp1.getOrDefault(word, 0) + 1);
+            mp.put(word, mp.getOrDefault(word, 0) + 1);
         }
-
         for (String word : words2) {
-            mp2.put(word, mp2.getOrDefault(word, 0) + 1);
-        }
-
-        int count = 0;
-
-        for (String word : words1) {
-            if (mp1.get(word) == 1 && mp2.getOrDefault(word, 0) == 1) {
-                count++;
+            Integer count = mp.get(word);
+            if (count != null && count < 2) {
+                mp.put(word, count - 1);
             }
         }
-
-        return count;
+        int ans = 0;
+        for (int count : mp.values()) {
+            if (count == 0) {
+                ans++;
+            }
+        }
+        return ans;
     }
 }
